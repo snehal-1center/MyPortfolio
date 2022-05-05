d71793df2c3fb9663d859d60cb2f6a5891709664
Snehal Patel <snehal.patel@1center.co>
2022-05-05 11:22:50 +0530

    aaa

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['media-exp1.licdn.com', 'miro.medium.com', 'images.unsplash.com', 'doodleipsum.com', ],
-    }
-  }
